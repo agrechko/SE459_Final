8d4609285a28b23d454e935afeb5864bd450d837
kirkspanski <kirkspanski@gmail.com>
2014-10-12 15:28:20 -0500

    Some xml parsing

--- RobotSweeper/src/sensors/FloorPlan.java
+++ RobotSweeper/src/sensors/FloorPlan.java
@@ -1,29 +1,86 @@
 package sensors;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.*;
 
 import objectsDTO.CellData;
 
-public class FloorPlan 
-{
-	ArrayList<ArrayList<CellData>> grid;//the full floor plan grid that is read in from the xml
-	ArrayList<ArrayList<CellData>> sweeperMemoryGrid;//the floor plan the sweeper encounters
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+
+public class FloorPlan {
+	
+	
+	HashMap<int[], CellData> fpmap = new HashMap<int[], CellData>();
+
+//	ArrayList<ArrayList<CellData>> grid;//the full floor plan grid that is read in from the xml
+//	ArrayList<ArrayList<CellData>> sweeperMemoryGrid;//the floor plan the sweeper encounters
 	
 	//floorPlanLocation is the physical drive location
-	public FloorPlan(String floorPlanLocation)
-	{
-		grid = new ArrayList<ArrayList<CellData>>();
-	}
+//	public FloorPlan(String floorPlanLocation)
+//	{
+//		grid = new ArrayList<ArrayList<CellData>>();
+//	}
+//	
+//	//write arraylist to xml to file
+//	public void writeFloorPlan(ArrayList<ArrayList<CellData>> grid)
+//	{
+//		
+//	}
+//	
+//	//prints arraylist to console
+//	public void printFloorPlan(ArrayList<ArrayList<CellData>> grid)
+//	{
+//		
+//	}
+//}
+
+
+public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
+	Logger log = Logger.getAnonymousLogger();
+	log.setLevel(Level.ALL);
+	ConsoleHandler hdlr = new ConsoleHandler();
+	hdlr.setLevel(Level.ALL);
+	SimpleFormatter logformat = new SimpleFormatter();
+	hdlr.setFormatter(logformat);
+	log.addHandler(hdlr);	
 	
-	//write arraylist to xml to file
-	public void writeFloorPlan(ArrayList<ArrayList<CellData>> grid)
-	{
-		
+	String inputfile = "sample_floorplan.xml";
+	String tags;
+	XMLInputFactory factory = XMLInputFactory.newInstance();
+//	XMLStreamReader reader = factory.createXMLStreamReader(ClassLoader.getSystemResourceAsStream("sample_floorplan.xml"));
+ 	XMLStreamReader reader;
+	try {
+		reader = factory.createXMLStreamReader(inputfile, new FileInputStream(inputfile));
+		while (reader.hasNext()) {
+			int event = reader.next();
+			switch(event) {
+	 		case XMLStreamConstants.START_ELEMENT:
+	            log.info("START_ELEMENT: " + reader.getLocalName());			
+				if (reader.getLocalName().equalsIgnoreCase("cell")) {
+					CellData c = new CellData();
+					int ac = reader.getAttributeCount();
+					for (int i=0; i < ac; i++) {
+						log.info(reader.getAttributeLocalName(i) + ", " + reader.getAttributeValue(i)); 
+					}
+//					log.info("CELL ATTRIBUTES: " + ac);
+				}
+				break;			
+			case XMLStreamConstants.END_ELEMENT:
+				break;
+			}
+		}
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	
-	//prints arraylist to console
-	public void printFloorPlan(ArrayList<ArrayList<CellData>> grid)
-	{
-		
-	}
+}
 }
