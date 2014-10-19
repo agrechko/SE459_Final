f64b04416868d5b7a392523b0f7a7f12cfe00040
Rahmo <rahmo.alzahrani@gmail.com>
2014-10-19 04:07:39 -0500

    TestFiles Not completed

---
+++ Simulation/src/main/java/sensors/SensorsControllerTest.java
@@ -0,0 +1,110 @@
+package sensors;
+import objectsDTO.CellData;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SensorsControllerTest {
+	String floorPlanLocation = "../Simulation/sample_floorplan.xml";
+	SensorsController sensor = new SensorsController("") ;
+	
+	@Test
+	public void TestConstructor() {
+		try{
+			//assertEquals( null,sensor. );
+        	  //fail();
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	  }
+	
+	@Test 
+	public  void getPathsTest(){
+		int x; int y;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+		
+	}
+	
+	@Test 
+	public void setPathsTest(){
+		int x; int y; int[] paths;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	}
+	
+	@Test 
+	public  void cleanTest(){
+		int x; int y;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	}
+	
+	@Test 
+	public void isCleanTest() {
+		int x; int y;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	}
+
+	
+	@Test 
+	public void getSurfaceTest(){
+		int x; int y;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+		
+	}
+	
+	
+	@Test 
+	public void setSurfaceTest(){
+		int x; int y; int surfaceValue;
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	}
+	
+	
+	@Test 
+	public  void isAllCleanTest(){
+		
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+		
+	}
+	
+	@Test 
+	public void getChargingStationLocationTest() {
+		
+		try{
+			
+          } catch ( IllegalArgumentException e ) { 
+        	    
+          }
+	
+	}
+	
+	
+}
