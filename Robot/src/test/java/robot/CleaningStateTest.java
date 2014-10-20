8c2b5a10b67ba80910d8087c78582c9243ce5c96
jimmykurian <kurianjimmy11@gmail.com>
2014-10-19 20:52:34 -0500

    Test for CleaningState

---
+++ Robot/src/test/java/robot/CleaningStateTest.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Jimmy
+ *
+ */
+public class CleaningStateTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link robot.CleaningState#execute(robot.RobotController)}.
+	 */
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented"); // TODO
+	}
+}
