33a2da932d2ca40d4f52b481d37656fb1ecd23ad
jimmykurian <kurianjimmy11@gmail.com>
2014-10-25 13:32:13 -0500

    Updates to CleaningStateTest - Added and Implemented Max Dirt Capacity and Low Power Tests - Added Floor Unit Already Clean and Is Cleaning Tests - Need to Implement Last Two Tests

--- Robot/src/test/java/statesTests/CleaningStateTest.java
+++ Robot/src/test/java/statesTests/CleaningStateTest.java
@@ -18,7 +18,7 @@ import robot.RobotController.State;
 import sensors.SensorsController;
 
 /**
- * @author Jimmy
+ * @author jimmykurian
  *
  */
 public class CleaningStateTest {
@@ -26,10 +26,10 @@ public class CleaningStateTest {
 	@SuppressWarnings("unchecked")
 	@Test
 	//Checks if Cleaning Capacity is at max, sends robot to go home
-	public void maxDirtCapacity()
+	public void maxDirtCapacityTest()
 	{
 		final int MAX_POWER = 50;
-		final int MAX_DIRT_CAPACITY = 0;
+		final int MAX_DIRT_CAPACITY = 50;
 		final int START_X = 0;
 		final int START_Y = 0;
 		
@@ -46,39 +46,56 @@ public class CleaningStateTest {
 		
 		robot.devpaths = (LinkedList<int[]>) testPaths.clone();
 		robot.setCurrentDirtCapacity(0);
+		robot.currentState = 2;
 		robot.run();
 		
 		assertTrue(robot.getCurrentDirtCapacity() == 0);
 		assertTrue(robot.currentState == State.GOING_HOME.getValue());
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Test method for {@link robot.CleaningState#execute(robot.RobotController)}.
-	 */
+	
+	@SuppressWarnings("unchecked")
 	@Test
-	public final void testExecute() {
-		fail("Not yet implemented"); // TODO
+	//Checks if power is low, sends robot to go home
+	public void lowPowerTest()
+	{
+		final int MAX_POWER = 50;
+		final int MAX_DIRT_CAPACITY = 50;
+		final int START_X = 0;
+		final int START_Y = 0;
+		
+		String floorPlanLocation = "./resources/floorPlan";
+		SensorsController sensors = new SensorsController(floorPlanLocation);
+		
+		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
+		robot.devModeOn = true;
+		
+		LinkedList<int[]> testPaths = new LinkedList<int[]>();
+		testPaths.add(new int[]{1,2,2,2});//(1,0)
+		testPaths.add(new int[]{2,2,1,2});//(1,1)
+		testPaths.add(new int[]{1,2,2,2});//(2,1)
+		testPaths.add(new int[]{2,2,1,2});//(2,2) low power state changed to going home
+		
+		robot.devpaths = (LinkedList<int[]>) testPaths.clone();
+		robot.currentState = 2;
+		robot.run();
+		
+		assertTrue(0 == robot.getCurrentPower());
+		assertTrue(robot.currentState == State.GOING_HOME.getValue());
 	}
-
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	//Checks if floor unit is already clean
+	public void floorUnitIsCleanTest()
+	{
+		//needs to be implemented
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	//Checks if robot is cleaning floor unit
+	public void isCleaningTest()
+	{
+		//needs to be implemented
+	}
 }
