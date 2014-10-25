a4c40db6b8a6aa0d92aad1b096e8666affd6c1c3
kirkspanski <kirkspanski@gmail.com>
2014-10-25 16:06:55 -0500

    Absolute path attempt

--- Robot/src/main/java/Main.java
+++ Robot/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.logging.Level;
 
 import robot.RobotController;
 import sensors.SensorsController;
+import utils.LogFactory;
 
 
 public class Main 
@@ -11,6 +13,8 @@ public class Main
 	final static int MAX_DIRT_CAPACITY = 50;
 	final static int START_X = 0;
 	final static int START_Y = 0;
+//    Loggelog = new LogFactory().generateLog("Floorplan", Level.INFO);
+
 		
 	public static void main(String[] args) throws Exception 
 	{
@@ -19,7 +23,10 @@ public class Main
 			floorPlanLocation = args[0];
 		
 		File f = new File(floorPlanLocation);
-		if(f.exists() && !f.isDirectory()) { System.out.println("file found"); }
+		if(f.exists() && !f.isDirectory()) { 
+			System.out.println("file found");
+			floorPlanLocation = f.getAbsolutePath();
+		}
 		
 		SensorsController sensors = new SensorsController(floorPlanLocation);
 		
@@ -28,23 +35,23 @@ public class Main
 		robot.start();
 		
 		Scanner scan = new Scanner(System.in);
-		while(true)
-		{
-			System.out.println("Available command:");
-			System.out.println("Print - prints the current state of the floor plan (NOT IMPLEMENTED)");
-			System.out.println("Stop - stops the current clean cycle");
-			String userCommand = scan.nextLine().toLowerCase().trim();
-			if(!robot.userInput(userCommand))
-			{
-				System.out.println("Command not recognized. Please try again.");
-			}
-			else
-			{
-				if(userCommand.equals("stop"))
-				{
-					break;
-				}
-			}
-		}
+//		while(true)
+//		{
+//			System.out.println("Available command:");
+//			System.out.println("Print - prints the current state of the floor plan (NOT IMPLEMENTED)");
+//			System.out.println("Stop - stops the current clean cycle");
+//			String userCommand = scan.nextLine().toLowerCase().trim();
+//			if(!robot.userInput(userCommand))
+//			{
+//				System.out.println("Command not recognized. Please try again.");
+//			}
+//			else
+//			{
+//				if(userCommand.equals("stop"))
+//				{
+//					break;
+//				}
+//			}
+//		}
 	}
 }
