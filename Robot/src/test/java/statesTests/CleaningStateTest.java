9b0e1730921d0192151c55ccb423ed7241b5f682
jimmykurian <kurianjimmy11@gmail.com>
2014-11-02 18:54:21 -0600

    Updates to CleaningStateTest

--- Robot/src/test/java/statesTests/CleaningStateTest.java
+++ Robot/src/test/java/statesTests/CleaningStateTest.java
@@ -28,23 +28,16 @@ public class CleaningStateTest {
 	//Checks if Cleaning Capacity is at max, sends robot to go home
 	public void maxDirtCapacityTest()
 	{
-		final int MAX_POWER = 50;
+		final int MAX_POWER = 12;
 		final int MAX_DIRT_CAPACITY = 50;
-		final int START_X = 0;
-		final int START_Y = 0;
-		
-		String floorPlanLocation = "./resources/floorPlan";
+		final int START_X = 2;
+		final int START_Y = 2;
+
+		String floorPlanLocation = "../Simulation/sample_floorplan_emptymeTest.xml";
 		SensorsController sensors = new SensorsController(floorPlanLocation);
 		
 		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
-		robot.devModeOn = true;
 		
-		LinkedList<int[]> testPaths = new LinkedList<int[]>();
-		testPaths.add(new int[]{1,2,2,2});//(1,0)
-		testPaths.add(new int[]{2,2,1,2});//(1,1)
-		testPaths.add(new int[]{1,2,2,2});//(2,1)
-		
-		robot.devpaths = (LinkedList<int[]>) testPaths.clone();
 		robot.setCurrentDirtCapacity(0);
 		robot.currentState = 2;
 		robot.run();
@@ -58,24 +51,16 @@ public class CleaningStateTest {
 	//Checks if power is low, sends robot to go home
 	public void lowPowerTest()
 	{
-		final int MAX_POWER = 50;
+		final int MAX_POWER = 0;
 		final int MAX_DIRT_CAPACITY = 50;
-		final int START_X = 0;
-		final int START_Y = 0;
-		
-		String floorPlanLocation = "./resources/floorPlan";
+		final int START_X = 4;
+		final int START_Y = 4;
+
+		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest1.xml";
 		SensorsController sensors = new SensorsController(floorPlanLocation);
 		
 		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
-		robot.devModeOn = true;
 		
-		LinkedList<int[]> testPaths = new LinkedList<int[]>();
-		testPaths.add(new int[]{1,2,2,2});//(1,0)
-		testPaths.add(new int[]{2,2,1,2});//(1,1)
-		testPaths.add(new int[]{1,2,2,2});//(2,1)
-		testPaths.add(new int[]{2,2,1,2});//(2,2) low power state changed to going home
-		
-		robot.devpaths = (LinkedList<int[]>) testPaths.clone();
 		robot.currentState = 2;
 		robot.run();
 		
