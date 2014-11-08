7071c792955697fdcc7976044df260368cc4fed5
jimmykurian <kurianjimmy11@gmail.com>
2014-11-07 21:05:43 -0600

    Fixed Tests in CleaningStateTest.java
    - All tests fully functional and passing.
    - Added floorIsAllCleanTest to CleaningStateTest.java

--- Robot/src/test/java/statesTests/CleaningStateTest.java
+++ Robot/src/test/java/statesTests/CleaningStateTest.java
@@ -4,6 +4,7 @@
 package statesTests;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
 
 import java.util.LinkedList;
 
@@ -28,22 +29,20 @@ public class CleaningStateTest {
 	//Checks if Cleaning Capacity is at max, sends robot to go home
 	public void maxDirtCapacityTest()
 	{
-		final int MAX_POWER = 12;
-		final int MAX_DIRT_CAPACITY = 50;
-		final int START_X = 2;
-		final int START_Y = 2;
-
-		String floorPlanLocation = "../Simulation/sample_floorplan_emptymeTest.xml";
+		final int MAX_POWER = 20;
+		final int MAX_DIRT_CAPACITY = 5;
+		final int START_X = 0;
+		final int START_Y = 0;
+		
+		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest4.xml";
 		SensorsController sensors = new SensorsController(floorPlanLocation);
 		
 		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
-		
-		robot.setCurrentDirtCapacity(0);
-		robot.currentState = 2;
+
 		robot.run();
-		
-		assertTrue(robot.getCurrentDirtCapacity() == 0);
-		assertTrue(robot.currentState == State.GOING_HOME.getValue());
+		assertTrue(robot.getCurrentX() == 0); 
+		assertTrue(robot.getCurrentY() == 0);
+		assertTrue(robot.getCurrentDirtCapacity() == 1);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -51,21 +50,21 @@ public class CleaningStateTest {
 	//Checks if power is low, sends robot to go home
 	public void lowPowerTest()
 	{
-		final int MAX_POWER = 0;
+		final int MAX_POWER = 4;
 		final int MAX_DIRT_CAPACITY = 50;
-		final int START_X = 4;
-		final int START_Y = 4;
+		final int START_X = 0;
+		final int START_Y = 0;
 
 		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest1.xml";
 		SensorsController sensors = new SensorsController(floorPlanLocation);
 		
 		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
 		
-		robot.currentState = 2;
 		robot.run();
 		
-		assertTrue(0 == robot.getCurrentPower());
-		assertTrue(robot.currentState == State.GOING_HOME.getValue());
+		//Checks if Robot has Returned home and not entered Cleaning State
+		assertTrue(robot.getCurrentX() == 0); 
+		assertTrue(robot.getCurrentY() == 0);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -73,7 +72,20 @@ public class CleaningStateTest {
 	//Checks if floor unit is already clean
 	public void floorUnitIsCleanTest()
 	{
-		//needs to be implemented
+		final int MAX_POWER = 8;
+		final int MAX_DIRT_CAPACITY = 50;
+		final int START_X = 0;
+		final int START_Y = 0;
+
+		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest1.xml";
+		SensorsController sensors = new SensorsController(floorPlanLocation);
+		
+		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
+		
+		robot.run();
+		assertTrue(robot.getCurrentX() == 0); 
+		assertTrue(robot.getCurrentY() == 0);
+		assertTrue(sensors.isClean(robot.getCurrentX(), robot.getCurrentY()) == true); 
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -81,6 +93,42 @@ public class CleaningStateTest {
 	//Checks if robot is cleaning floor unit
 	public void isCleaningTest()
 	{
-		//needs to be implemented
+		final int MAX_POWER = 20;
+		final int MAX_DIRT_CAPACITY = 5;
+		final int START_X = 0;
+		final int START_Y = 0;
+			
+		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest4.xml";
+		SensorsController sensors = new SensorsController(floorPlanLocation);
+			
+		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
+
+		assertTrue(sensors.isClean(2, 1) == false);
+		robot.run();
+		assertTrue(robot.getCurrentX() == 0); 
+		assertTrue(robot.getCurrentY() == 0);
+		assertTrue(sensors.isClean(2, 1) == true);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	//Checks if the entire Floor has been cleaned
+	public void entireFloorIsCleanTest()
+	{
+		final int MAX_POWER = 20;
+		final int MAX_DIRT_CAPACITY = 5;
+		final int START_X = 0;
+		final int START_Y = 0;
+			
+		String floorPlanLocation = "../Simulation/sample_floorplan_exploreTest4.xml";
+		SensorsController sensors = new SensorsController(floorPlanLocation);
+			
+		RobotController robot = new RobotController(sensors, MAX_POWER, MAX_DIRT_CAPACITY, START_X, START_Y);
+
+		assertTrue(sensors.isAllClean() == false);
+		robot.run();
+		assertTrue(robot.getCurrentX() == 0); 
+		assertTrue(robot.getCurrentY() == 0);
+		assertTrue(sensors.isAllClean() == true);
 	}
 }
