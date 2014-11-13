1ee9fe2b3b79c213feb39bc71c1b6848703305fc
agrechko <agrechko@yahoo.com>
2014-11-12 21:51:02 -0600

    looger added instead of out to console

--- Robot/src/main/java/robot/CleaningState.java
+++ Robot/src/main/java/robot/CleaningState.java
@@ -1,9 +1,12 @@
 package robot;
 
 import robot.RobotController.State;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CleaningState implements RobotStates {
 	int surface; 
+	Logger logger = Logger.getLogger("main");
 	
 	public void execute(RobotController robot) {
 		if(robot.getCurrentDirtCapacity() == 0) {
@@ -16,13 +19,13 @@ public class CleaningState implements RobotStates {
 			} else if(robot.currentPower - robot.getPowerConsumption(robot.sensors.getSurface(robot.currentX, robot.currentY)) <= (robot.maxPower/2)) {
 				robot.currentState = State.GOING_HOME.getValue();
 			} else {
-				System.out.println("before "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
+				logger.log(Level.FINE, "before "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
 				surface = robot.sensors.getSurface(robot.currentX, robot.currentY);
 				robot.sensors.clean(robot.currentX, robot.currentY);
 				robot.setCurrentDirtCapacity(robot.getCurrentDirtCapacity() - 1); 
 				robot.currentPower = robot.currentPower - robot.getPowerConsumption(surface);
-				System.out.println("After "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
-				System.out.println("current dirt capacity "+ robot.getCurrentDirtCapacity());
+				logger.log(Level.FINE, "After "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
+				logger.log(Level.FINE, "current dirt capacity "+ robot.getCurrentDirtCapacity());
 			}
 		}
 	}
