b7111278c5b33700811f1d1ba43890c7f42f2837
jimmykurian <kurianjimmy11@gmail.com>
2014-11-12 21:15:59 -0600

    SonarQube Fixes for CleaningState.java - Fixes for some of the violations listed by SonarQube for CleaningState.java

--- Robot/src/main/java/robot/CleaningState.java
+++ Robot/src/main/java/robot/CleaningState.java
@@ -1,40 +1,30 @@
 package robot;
 
 import robot.RobotController.State;
-import sensors.SensorsController;
 
-public class CleaningState implements RobotStates
-{
+public class CleaningState implements RobotStates {
 	int surface; 
 	
-	public void execute(RobotController robot) 
-	{
-		if(robot.getCurrentDirtCapacity() == 0)
-		{
+	public void execute(RobotController robot) {
+		if(robot.getCurrentDirtCapacity() == 0) {
 			robot.currentState = State.GOING_HOME.getValue();
 		}
-		else if(robot.currentPower <= (robot.maxPower/2))
-		{
+		else if(robot.currentPower <= (robot.maxPower/2)) {
 			robot.currentState = State.GOING_HOME.getValue();
 		}
 		else
 		{
-			if(robot.sensors.isClean(robot.currentX, robot.currentY) == true)
-			{
+			if(robot.sensors.isClean(robot.currentX, robot.currentY) == true) {
 				robot.currentState = State.EXPLORING.getValue();
 			}
-			else if(robot.currentPower - robot.getPowerConsumption(robot.sensors.getSurface(robot.currentX, robot.currentY)) <= (robot.maxPower/2))
-			{
-				//added this else if because of the case that we clean a cell and power become less than half
+			else if(robot.currentPower - robot.getPowerConsumption(robot.sensors.getSurface(robot.currentX, robot.currentY)) <= (robot.maxPower/2)) {
 				robot.currentState = State.GOING_HOME.getValue();
 			}
-			else
-			{
+			else {
 				System.out.println("before "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
 				surface = robot.sensors.getSurface(robot.currentX, robot.currentY);
 				robot.sensors.clean(robot.currentX, robot.currentY);
 				robot.setCurrentDirtCapacity(robot.getCurrentDirtCapacity() - 1); 
-				//robot.currentPower = robot.currentPower - surface;//incorrect because high carpet will cost 4 units this way but should be 3
 				robot.currentPower = robot.currentPower - robot.getPowerConsumption(surface);
 				System.out.println("After "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
 				System.out.println("current dirt capacity "+ robot.getCurrentDirtCapacity());
