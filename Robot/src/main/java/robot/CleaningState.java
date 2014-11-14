d2c54eb1090803facd7ff3543653a5a9aef56f51
jimmykurian <kurianjimmy11@gmail.com>
2014-11-14 09:35:48 -0600

    Replaced all tab characters in CleaningState.java by sequences of white spaces. - Fix for "tabulation characters should not be used" issue in SonarQube - Used Checkstyle 6.1 Plugin for Eclipse to convert tab characters to whitespaces.

--- Robot/src/main/java/robot/CleaningState.java
+++ Robot/src/main/java/robot/CleaningState.java
@@ -1,32 +1,48 @@
 package robot;
 
-import robot.RobotController.State;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import robot.RobotController.State;
+
 public class CleaningState implements RobotStates {
-	int surface; 
-	Logger logger = Logger.getLogger("main");
-	
-	public void execute(RobotController robot) {
-		if(robot.getCurrentDirtCapacity() == 0) {
-			robot.currentState = State.GOING_HOME.getValue();
-		} else if(robot.currentPower <= (robot.maxPower/2)) {
-			robot.currentState = State.GOING_HOME.getValue();
-		} else {
-			if(robot.sensors.isClean(robot.currentX, robot.currentY)) {
-				robot.currentState = State.EXPLORING.getValue();
-			} else if(robot.currentPower - robot.getPowerConsumption(robot.sensors.getSurface(robot.currentX, robot.currentY)) <= (robot.maxPower/2)) {
-				robot.currentState = State.GOING_HOME.getValue();
-			} else {
-				logger.log(Level.FINE, "before "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
-				surface = robot.sensors.getSurface(robot.currentX, robot.currentY);
-				robot.sensors.clean(robot.currentX, robot.currentY);
-				robot.setCurrentDirtCapacity(robot.getCurrentDirtCapacity() - 1); 
-				robot.currentPower = robot.currentPower - robot.getPowerConsumption(surface);
-				logger.log(Level.FINE, "After "+robot.sensors.getCell(robot.currentX, robot.currentY).getDirt());
-				logger.log(Level.FINE, "current dirt capacity "+ robot.getCurrentDirtCapacity());
-			}
-		}
-	}
+    int surface;
+    Logger logger = Logger.getLogger("main");
+
+    public final void execute(final RobotController robot) {
+        if (robot.getCurrentDirtCapacity() == 0) {
+            robot.currentState = State.GOING_HOME.getValue();
+        } else if (robot.currentPower <= (robot.maxPower / 2)) {
+            robot.currentState = State.GOING_HOME.getValue();
+        } else {
+            if (robot.sensors.isClean(robot.currentX, robot.currentY)) {
+                robot.currentState = State.EXPLORING.getValue();
+            } else if (robot.currentPower
+                    - robot.getPowerConsumption(robot.sensors.getSurface(
+                            robot.currentX, robot.currentY)) <= (robot.maxPower / 2)) {
+                robot.currentState = State.GOING_HOME.getValue();
+            } else {
+                logger.log(
+                        Level.FINE,
+                        "before "
+                                + robot.sensors.getCell(robot.currentX,
+                                        robot.currentY).getDirt());
+                surface = robot.sensors.getSurface(robot.currentX,
+                        robot.currentY);
+                robot.sensors.clean(robot.currentX, robot.currentY);
+                robot.setCurrentDirtCapacity(robot.getCurrentDirtCapacity() - 1);
+                robot.currentPower = robot.currentPower
+                        - robot.getPowerConsumption(surface);
+                logger.log(
+                        Level.FINE,
+                        "After "
+                                + robot.sensors.getCell(robot.currentX,
+                                        robot.currentY).getDirt());
+                logger.log(
+                        Level.FINE,
+                        "current dirt capacity "
+                                + robot.getCurrentDirtCapacity());
+            }
+        }
+    }
 }
